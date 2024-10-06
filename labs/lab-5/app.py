5f7afe582a645c6e5858496870ea33b01efcae6a
owentee1 <147005571+owentee1@users.noreply.github.com>
2024-10-06 16:05:58 -0400

    completed lab-5

--- labs/lab-5/app.py
+++ labs/lab-5/app.py
@@ -8,6 +8,22 @@ from db.server import app
 def index():
     return render_template("index.html")
 
+@app.route('/bad1')
+def bad1():
+    return render_template("bad1.html")
+
+@app.route('/mightSpace_help')
+def mightSpace_help():
+    return render_template("mightSpace_help.html")
+
+@app.route('/resources')
+def resources():
+    return render_template("resources.html")
+
+@app.route('/bad4')
+def bad4():
+    return render_template("bad4.html")
+
 if __name__ == "__main__":
     # debug refreshes your application with your new changes every time you save
     app.run(debug=True)
