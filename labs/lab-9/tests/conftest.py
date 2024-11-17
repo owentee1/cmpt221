6a3ff30f13a1f21cd03c32553d1db13536c80442
owentee1 <147005571+owentee1@users.noreply.github.com>
2024-11-17 08:15:07 -0500

    lab-9 complete

--- labs/lab-9/tests/conftest.py
+++ labs/lab-9/tests/conftest.py
@@ -36,10 +36,46 @@ def db_session():
 # example fixture - user sign in input
 # hint... can you do something similar for login?
 @pytest.fixture
-def sample_signup_input():
-    return {'FirstName': 'Calista', 
-            'LastName': 'Phippen', 
-            'Email': 'calista.phippen1@marist.edu', 
-            'PhoneNumber': '1234567891', 
-            'Password': 'mypassword'
+def sample_signup_owen():
+    return {'FirstName': 'Owen', 
+            'LastName': 'Thomas', 
+            'Email': 'owen.thomas1@marist.edu', 
+            'PhoneNumber': '8888888888', 
+            'Password': 'strongestPasswordEver'
+            }
+
+@pytest.fixture
+def sample_signup_alex():
+    return {'FirstName': 'Alex', 
+            'LastName': 'Thomas', 
+            'Email': 'alex.downey1@marist.edu', 
+            'PhoneNumber': '7777777777', 
+            'Password': 'strongestPasswordEver1'
+            }
+
+@pytest.fixture
+def sample_signup_austin():
+    return {'FirstName': 'Austin', 
+            'LastName': 'James', 
+            'Email': 'Austin.james1@marist.edu', 
+            'PhoneNumber': '6666666666', 
+            'Password': 'strongestPasswordEver2'
+            }
+
+@pytest.fixture
+def sample_signup_georgia():
+    return {'FirstName': 'Georgia', 
+            'LastName': 'Angelina', 
+            'Email': 'georgia.angelina1@marist.edu', 
+            'PhoneNumber': '5555555555', 
+            'Password': 'strongestPasswordEver3'
+            }
+
+@pytest.fixture
+def sample_signup_mike():
+    return {'FirstName': 'Mike', 
+            'LastName': 'Corbi', 
+            'Email': 'mike.corbi1@marist.edu', 
+            'PhoneNumber': '4444444444', 
+            'Password': 'strongestPasswordEver9'
             }
--- labs/lab-9/tests/test.py
+++ labs/lab-9/tests/test.py
@@ -11,8 +11,8 @@ def test_db_connection(db_session):
 
 # test to insert a user
 # you can count this as one of your 5 test cases :)
-def test_insert_user(db_session, sample_signup_input):
-    insert_stmt = insert(User).values(sample_signup_input)
+def test_insert_user(db_session, sample_signup_owen):
+    insert_stmt = insert(User).values(sample_signup_owen)
 
     # execute insert query
     db_session.execute(insert_stmt)
@@ -20,7 +20,73 @@ def test_insert_user(db_session, sample_signup_input):
     db_session.commit()
 
     # not part of the app.py code, just being used to get the inserted data
-    selected_user = db_session.query(User).filter_by(FirstName="Calista").first()
+    selected_user = db_session.query(User).filter_by(FirstName="Owen").first()
 
     assert selected_user is not None
-    assert selected_user.LastName == "Phippen"
+    assert selected_user.LastName == "Thomas"
+
+#tests how it handles duplicate email submissions
+def test_duplicate_email(db_session, sample_signup_alex):
+    #insert first user
+    insert_stmt = insert(User).values(sample_signup_alex)
+    # execute insert query
+    db_session.execute(insert_stmt)
+    # commit the changes to the db
+    db_session.commit()
+
+    #insert second user by copying the "alex fixture"
+    duplicate_user_email = sample_signup_alex.copy()
+    duplicate_user_email["FirstName"] = "Duplicate"
+    db_session.execute(insert(User).values(duplicate_user_email))
+    db_session.commit()
+
+    #checks to see if there are two users with same email
+    users_with_duplicate_email = db_session.query(User).filter_by(Email = sample_signup_alex["Email"]).all()
+
+    assert len(users_with_duplicate_email) == 2
+    assert users_with_duplicate_email[0].FirstName == "Alex"
+    assert users_with_duplicate_email[1].FirstName == "Duplicate"
+
+#this is my expected to fail test, it attempts to find a user that does not exist.
+@pytest.mark.xfail
+def test_user_DNE(db_session):
+
+    selected_user = db_session.query(User).filter_by(FirstName = "Nobody").first()
+
+    assert selected_user is not None, "Expected fail because user 'Nobody' DNE. "
+
+#Tests finding a user by first name/last name
+def test_find_user_by_last_name(db_session, sample_signup_alex):
+
+    insert_stmt = insert(User).values(sample_signup_alex)
+    db_session.execute(insert_stmt)
+    db_session.commit()
+
+    #find user by full name and email I added email because I had multiple "Alex Thomas in my db and it was failing. This is better practice anyway, large dbs probably have multiple people with the same name, an email is a good way to distinguish them"
+    selected_user = db_session.query(User).filter_by(LastName="Thomas", FirstName="Alex", Email=sample_signup_alex["Email"]).first()
+   
+    assert selected_user is not None, "User with first name 'Alex and last name 'Thomas' should exist."
+    assert selected_user.Email == sample_signup_alex["Email"]
+
+#this test determines whether a required '@' symbol was used when entering email
+def test_for_at_symbol(db_session, sample_signup_georgia):
+
+    insert_stmt = insert(User).values(sample_signup_georgia)
+    db_session.execute(insert_stmt)
+    db_session.commit()
+
+    # verifies email format is correct.
+    selected_user = db_session.query(User).filter_by(FirstName = "Georgia").first()
+    assert selected_user is not None
+    assert "@" in selected_user.Email
+
+@pytest.mark.xfail(reason = "Expected failure due to missing table")
+def test_critical_error_table_DNE(db_session):
+
+    #query for a table that doesnt exist in the db
+    non_exist_table_query = text("SELECT * FROM non_exist_table")
+
+    #execute the query, expected to fail cause table dne.
+    db_session.execute(non_exist_table_query)
+
+    assert False, "Critical Error due to querying a table that is non-existent"
